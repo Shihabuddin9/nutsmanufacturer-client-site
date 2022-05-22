06b1ef8e09a519554586b42095c38472699dc2af
Shihabuddin9 <shihab.bd.pk@gmail.com>
2022-05-22 20:14:42 +0600

    scroll botton to top add

--- src/pages/Shared/Footer.js
+++ src/pages/Shared/Footer.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ScrollToTop from "react-scroll-to-top";
 
 const Footer = () => {
     return (
         <footer class="footer p-10 bg-neutral text-neutral-content">
+            <ScrollToTop smooth top="100" width='40' height='30' viewBox="20 -60 256 256" />
             <div>
                 <span class="footer-title">Services</span>
                 <a class="link link-hover">Branding</a>
